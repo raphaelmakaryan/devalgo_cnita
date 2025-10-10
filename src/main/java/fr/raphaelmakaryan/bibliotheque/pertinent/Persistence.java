30663cfb39feefc64c0cbe5eb6925d9bc94b4745
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-10-10 10:40:46 +0200

    Ajout de graddle

---
+++ src/main/java/fr/raphaelmakaryan/bibliotheque/pertinent/Persistence.java
@@ -0,0 +1,23 @@
+package fr.raphaelmakaryan.bibliotheque.pertinent;
+/*
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.client.*;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public interface Persistence {
+    String connectionString = "mongodb+srv://raph:ZtpI0vAe2RoDg3AQ@campusnumerique.8msqkrp.mongodb.net/?retryWrites=true&w=majority&appName=campusnumerique";
+}
