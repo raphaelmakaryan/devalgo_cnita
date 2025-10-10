5d213c972f62ce37d6e16ff4d54efccaded4199f
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-10-10 10:10:04 +0200

    Ajout du multi-jeux, permet de créer un jeu personnalisé

---
+++ src/fr/raphaelmakaryan/bibliotheque/controllers/CustomGameController.java
@@ -0,0 +1,9 @@
+package fr.raphaelmakaryan.bibliotheque.controllers;
+
+import fr.raphaelmakaryan.bibliotheque.modeles.GameModele;
+
+public class CustomGameController extends GameController {
+    public CustomGameController(GameModele game) {
+        super(game);
+    }
+}
---
+++ src/fr/raphaelmakaryan/bibliotheque/modeles/CustomGame.java
@@ -0,0 +1,7 @@
+package fr.raphaelmakaryan.bibliotheque.modeles;
+
+public class CustomGame extends GameModele {
+    public CustomGame(int size, int victoryValue) {
+        super(size, victoryValue);
+    }
+}
